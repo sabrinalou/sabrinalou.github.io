fbbf9fda1cffe3ca06bccce438d0c575e6f01c76
sabrinalou3 <87052249+sabrinalou3@users.noreply.github.com>
2026-02-11 00:26:11 -0800

    set up react

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Markl</h1>
+    <p>Shrimpster</p>
+  </React.StrictMode>
+);
